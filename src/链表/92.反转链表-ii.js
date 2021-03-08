b28ddc8da5ded8a8d64a29dfddd31f6bb70a54c0
ygg29 <xiaoyou29@163.com>
2021-03-08 15:08:53 +0800

    update

--- src/链表/92.反转链表-ii.js
+++ src/链表/92.反转链表-ii.js
@@ -41,6 +41,24 @@
  */
 var reverseBetween = function(head, left, right) {
 
+  // 返回翻转后的链表的头结点
+  var reverseN = function(head, n) {
+    if (n == 1) {return head;}
+    var newList = reverseN(head.next, n-1); // 转换完成的链表
+    var tail = head.next;  // 翻过过后的链表尾结点(也是未翻转链表的尾节点， 细评)
+    head.next = tail.next; // 将链表的尾节点记录下来， 递归赋值给翻转后链表的尾节点
+    tail.next = head;  // 指针翻转
+    return newList;
+  }
+  let hair = new ListNode(-1, head);
+  var p = hair;
+  let cnt = right-left+1;
+  while(--left){
+    p = p.next; //移动到需要翻转区间的起点
+  }
+  p.next = reverseN(p.next, cnt); // 将 p 的 next 指向新链表的头结点
+  return vir.next;
+
 };
 // @lc code=end
 
