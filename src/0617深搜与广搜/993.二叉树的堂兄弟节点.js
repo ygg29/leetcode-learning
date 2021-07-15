126f24d910bb2ec4a57cbb853ff68391837c7a6f
肖友 <xiaoyou29@163.com>
2021-07-15 21:11:08 +0800

    update

---
+++ src/0617深搜与广搜/993.二叉树的堂兄弟节点.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=993 lang=javascript
+ *
+ * [993] 二叉树的堂兄弟节点
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean}
+ */
+var isCousins = function(root, x, y) {
+
+};
+// @lc code=end
+
